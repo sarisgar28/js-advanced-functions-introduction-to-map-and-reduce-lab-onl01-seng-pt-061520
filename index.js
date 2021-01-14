6d25f359e50541a0e22d5524eee8d6121face933
sarisgar28 <sarisgar28@gmail.com>
2021-01-14 13:21:08 -0500

    commit tree a895aa11311f2bca494b48eb9bf724652ab5d29b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+  let newArr = []
+    for (let num = 0; num  < sourceArray.length; num++){
+   newArr.push(sourceArray[num] * -1)
+  }
+      return newArr
+}
+
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i]) 
+  }
+  return r
+}
+ 
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i]) 
+  }
+  return r
+}
+ 
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i]) 
+  }
+  return r
+}
+  
